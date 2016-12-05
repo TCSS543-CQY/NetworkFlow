101fccaa19fa489cf2d1fbb260acd979cf7bf5c3
zijunqu@gmail.com <zijunqu@Zijuns-MacBook-Pro.local>
2016-12-05 00:05:22 -0800

    Add Fordfulkerson

--- src/residualgraph/ResidualGraph.java
+++ src/residualgraph/ResidualGraph.java
@@ -1,7 +1,27 @@
 package residualgraph;
-
+import java.util.*;
 /**
  * @author zijun,weijia,annie
  */
 public class ResidualGraph {
+    LinkedList vertexList;
+    LinkedList edgeList;
+
+
+    /**
+     * construct list of vertices and edges of residual graph
+     */
+    public ResidualGraph()
+    {
+       vertexList = new LinkedList();
+        edgeList = new LinkedList();
+    }
+    public Iterator vertices() {
+        return vertexList.iterator();
+    }
+    public Iterator edges() {
+        return edgeList.iterator();
+    }
+
+
 }
