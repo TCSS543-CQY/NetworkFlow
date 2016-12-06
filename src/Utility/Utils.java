ef8e92f30ae471e1a2e15033d9df822cc1a8dec0
annieyan <annieyan9033@hotmail.com>
2016-12-06 15:36:50 -0800

    test change

--- src/Utility/Utils.java
+++ src/Utility/Utils.java
@@ -16,7 +16,7 @@ public class Utils {
 		BigDecimal SUM, AVERAGE;
 	
            
-		long sum = 0;
+		double sum = 0.0;
 	
 		for(int i=0; i < runtime.length ; i++)
             sum = sum + runtime[i];
